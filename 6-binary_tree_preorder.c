8a6d7acb7e5e069793b6b30c40570658636d927c
DouibaOmar <douibaomar40@gmail.com>
2023-11-28 18:17:21 +0100

    Done

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -11,7 +11,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int))
 {
 	if (tree)
 	{
-		func(tree->n);
+		if (func)
+			func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 	}
