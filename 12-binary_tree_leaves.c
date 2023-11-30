1b056d9aac2a3c15255f1f0ebfb4223e1a9975b9
DouibaOmar <douibaomar40@gmail.com>
2023-11-30 16:36:04 +0100

    Done

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,8 +8,8 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (!tree)
-		return 0;
+		return (0);
 	if (!tree->right && !tree->left)
-		return 1;
+		return (1);
 	return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
 }
